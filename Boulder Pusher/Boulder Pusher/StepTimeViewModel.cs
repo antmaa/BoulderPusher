5975778a52bf4911b1ce3b46a36af5ae5192e71e
Bjorter <bjorkman.tero@gmail.com>
2016-04-19 09:28:57 +0300

    Step counter and time works?

---
+++ Boulder Pusher/Boulder Pusher/StepTimeViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boulder_Pusher
+{
+    class StepTimeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public StepTimeViewModel()
+        {
+            step = 0;
+            time = 0;
+        }
+
+        public int step
+        {
+            get { return step; }
+            set
+            {
+                step = value;
+                RaisePropertyChanged();
+            }
+        }
+        public int time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
