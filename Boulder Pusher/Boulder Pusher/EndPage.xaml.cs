b364493701e4eb436687976344434c2146cde939
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-04-26 10:01:32 +0300

    Added EndPage.

---
+++ Boulder Pusher/Boulder Pusher/EndPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Boulder_Pusher
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EndPage : Page
+    {
+
+        // Canvas values
+        private double CanvasWidth = 550;
+        private double CanvasHeight = 550;
+
+        public EndPage()
+        {
+            this.InitializeComponent();
+
+            // Window Size
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.PreferredLaunchViewSize = new Size(1280, 720);
+
+            // Used in Ball and Paddle
+            CanvasWidth = MyCanvas.Width;
+            CanvasHeight = MyCanvas.Height;
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to GamePage
+            this.Frame.Navigate(typeof(GamePage));
+        }
+
+        private void MmButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to MainPage
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Exits the application
+            Application.Current.Exit();
+        }
+    }
+}
